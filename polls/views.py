27f1ae48974d3502d885f2b0033dc918ac95d660
Shuo Sun <shuo.sun@veeva.com>
2022-04-03 17:43:27 +0800

    Change vote detail and result page style

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,9 @@
 from multiprocessing import context
-import re
-from urllib import response
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.http import Http404
-from django.template import loader
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -15,34 +12,25 @@ def index(request):
     # render 函数的作用：「载入模板，填充上下文，再返回由它生成的 HttpResponse 对象」
     return render(request, 'polls/index.html', context)
 
-    # version-03
-    # template = loader.get_template('polls/index.html')
-    # context = {
-    #     'latest_question_list': latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-
-    # version-02: 
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-
-    # version-01: 
-    # return HttpResponse("Hello, world. You're at the polls index.")
-
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # version-02
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
-    # version-01
-    # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # reverse() 这个函数避免了我们在视图函数中硬编码 URL
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
